54e2e7ea5c625fdeed11e6e2d3036fc140c033e3
duonghongtuan96 <tuan.dh@agiletech.vn>
2021-11-18 21:48:43 +0700

    nop bao cao lab02

--- code/ecobike/src/application/Main.java
+++ code/ecobike/src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			//BorderPane root = new BorderPane();
-			Parent root = FXMLLoader.load(getClass().getResource("/views/home.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/views/KetQuaTraXe.fxml"));
 			Scene scene = new Scene(root);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
